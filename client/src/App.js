03ac9452ee7ca761cbcd81fae1f84283b572aa84
ajfrierson <alvinfrierson@fuller.edu>
2019-02-26 13:33:37 -0800

    included a react component and styling.

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route} from 'react-router-dom';
+import axios from 'axios';
+import Athentication from './components/Authentication';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    }
+  }
+  componentDidMount() {
+    axios.get('http://localhost:8000/api/users').then(response => {
+      this.setState({users: response.data})
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+
   render() {
+    console.log(this.state.users)
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
+        <div>
+          {this.state.users.length > 0 ? this.state.users.map(user => {
+            return <div key={user.id}>{user.user.name}</div>
+          }): 'Loading...'}
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default Authentication(App);
